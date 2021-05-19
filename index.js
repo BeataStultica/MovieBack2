a2792ed5923d9905b16770dc6ca611807cfc2a44
Dmytro Ukrainets <dimon2000.1982@gmail.com>
2021-05-19 22:22:19 +0300

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(process.env.PORT || 5000, (err) => {
+app.listen(process.env.PORT || 5000, "0.0.0.0", (err) => {
   console.log("Listening on " + process.env.PORT);
 });
