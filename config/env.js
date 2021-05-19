d070b65aa9a9af99701615c56b7b458b390a3560
Dmytro Ukrainets <dimon2000.1982@gmail.com>
2021-05-19 19:15:28 +0300

    fix pool

---
+++ config/env.js
@@ -0,0 +1,16 @@
+const env = {
+  database: "da03vvrv0hhp7l",
+  username: "lmiadzbwcpstsz",
+  password: "f39cae1f7f55cd3cb16010e924eab745533c1eb9568e04f35ee92ea21ae0afa8",
+  host: "ec2-54-216-185-51.eu-west-1.compute.amazonaws.com",
+  port: 5432,
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+
+module.exports = env;
--- src/queries/pool.js
+++ src/queries/pool.js
@@ -1,18 +1,24 @@
 const { Pool } = require("pg");
+const env = require("./../../config/env");
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 console.log(process.env.DATABASE_URL);
-const db =
-  process.env.NODE_ENV === "test" ? "movieplaza_test" : "da03vvrv0hhp7l";
+//const db =
+//  process.env.NODE_ENV === "test" ? "movieplaza_test" : "da03vvrv0hhp7l";
 const pool = new Pool({
   //connectionString: process.env.DATABASE_URL,
-  user: "lmiadzbwcpstsz",
-  host: "ec2-54-216-185-51.eu-west-1.compute.amazonaws.com",
-  database: db,
-  password: "f39cae1f7f55cd3cb16010e924eab745533c1eb9568e04f35ee92ea21ae0afa8",
-  port: 5432,
+  user: env.username,
+  host: env.host,
+  database: env.database,
+  password: env.password,
+  port: env.port,
   ssl: true,
   //ssl: { rejectUnauthorized: false },
 });
-pool.connect();
+pool.connect((err) => {
+  if (err) {
+    console.log(err);
+    return console.error("Error acquiring pool", err.stack);
+  }
+});
 
 module.exports = pool;
