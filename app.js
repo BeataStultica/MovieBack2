b92f4725cfc7b034094607d6dacf08da2e691244
Dmytro Ukrainets <dimon2000.1982@gmail.com>
2021-05-19 22:07:26 +0300

    first commit

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const fastify = require('fastify');
+const fastify = require("fastify");
 const app = fastify();
-const fastifySession = require('fastify-session');
-const fastifyCookie = require('fastify-cookie');
+const fastifySession = require("fastify-session");
+const fastifyCookie = require("fastify-cookie");
 app.register(fastifyCookie);
 
-app.register(require('fastify-socket.io'), {
-    cors: { origin: '*' },
+app.register(require("fastify-socket.io"), {
+  cors: { origin: "*" },
 });
 
-const pgSession = require('connect-pg-simple')(fastifySession);
+const pgSession = require("connect-pg-simple")(fastifySession);
 
 app.register(fastifySession, {
-    store: new pgSession({
-        conString:
-            'postgres://movieadmin1:movieadmin@localhost:5432/movieplaza',
-        tableName: 'session',
-    }),
-    cookieName: 'sessionId',
-    secret: '1qwqwqwwhjehu2372e8ywhdhu92e8uids',
-    cookie: { secure: false, path: '/', maxAge: 7 * 24 * 60 * 60 * 1000 }, //срок дії cookie 7 днів, протокол http(secure: false)
+  store: new pgSession({
+    conString: "postgres://movieadmin1:movieadmin@localhost:5432/movieplaza",
+    tableName: "session",
+  }),
+  cookieName: "sessionId",
+  secret: "1qwqwqwwhjehu2372e8ywhdhu92e8uids",
+  cookie: { secure: false, path: "/", maxAge: 7 * 24 * 60 * 60 * 1000 }, //срок дії cookie 7 днів, протокол http(secure: false)
 });
 
+/*
 app.register(require('fastify-cors'), {
     origin: 'http://localhost:3000',
 
@@ -36,20 +36,20 @@ app.register(require('fastify-cors'), {
         'Cookie',
     ],
 });
-
-app.register(require('./src/routes/index'));
-app.register(require('./src/routes/login'));
-app.register(require('./src/routes/filmImg'));
-app.register(require('./src/routes/filmpage'));
-app.register(require('./src/routes/newfilms'));
-app.register(require('./src/routes/filmsearch'));
-app.register(require('./src/routes/users'));
-app.register(require('./src/routes/profile'));
-app.register(require('./src/routes/videos'));
-app.register(require('./src/routes/registration'));
-app.register(require('./src/routes/commentadd'));
-app.register(require('./src/routes/update_user'));
-const sock = require('./src/sockets')(app);
+*/
+app.register(require("./src/routes/index"));
+app.register(require("./src/routes/login"));
+app.register(require("./src/routes/filmImg"));
+app.register(require("./src/routes/filmpage"));
+app.register(require("./src/routes/newfilms"));
+app.register(require("./src/routes/filmsearch"));
+app.register(require("./src/routes/users"));
+app.register(require("./src/routes/profile"));
+app.register(require("./src/routes/videos"));
+app.register(require("./src/routes/registration"));
+app.register(require("./src/routes/commentadd"));
+app.register(require("./src/routes/update_user"));
+const sock = require("./src/sockets")(app);
 app.ready(sock);
 
 module.exports = app;
