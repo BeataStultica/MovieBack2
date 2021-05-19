e5b2a073b63afff64f60b1b53f21583a4a4d31a0
Dmytro Ukrainets <dimon2000.1982@gmail.com>
2021-05-19 18:49:27 +0300

    fix pool

--- src/queries/pool.js
+++ src/queries/pool.js
@@ -2,13 +2,13 @@ const { Pool } = require("pg");
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 console.log(process.env.DATABASE_URL);
 const db =
-  process.env.NODE_ENV === "test" ? "movieplaza_test" : "dboqj906ps4mnl";
+  process.env.NODE_ENV === "test" ? "movieplaza_test" : "da03vvrv0hhp7l";
 const pool = new Pool({
   //connectionString: process.env.DATABASE_URL,
-  user: "bshemyphktauco",
-  host: "ec2-34-255-134-200.eu-west-1.compute.amazonaws.com",
+  user: "lmiadzbwcpstsz",
+  host: "ec2-54-216-185-51.eu-west-1.compute.amazonaws.com",
   database: db,
-  password: "97f8bea4d382c99b4749da4440914853debec42bdc83771aeaf13c1a7b1822a5",
+  password: "f39cae1f7f55cd3cb16010e924eab745533c1eb9568e04f35ee92ea21ae0afa8",
   port: 5432,
   ssl: true,
   //ssl: { rejectUnauthorized: false },
