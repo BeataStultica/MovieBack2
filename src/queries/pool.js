27eeb30b0958c142866f19cceeae79b326c1f3e7
Dmytro Ukrainets <dimon2000.1982@gmail.com>
2021-05-19 20:21:28 +0300

    fix pool

--- src/queries/pool.js
+++ src/queries/pool.js
@@ -1,18 +1,21 @@
-const { Pool, Client } = require("pg");
+const { Pool } = require("pg");
 const env = require("./../../config/env");
 //process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 console.log(process.env.DATABASE_URL);
 //const db =
 //  process.env.NODE_ENV === "test" ? "movieplaza_test" : "da03vvrv0hhp7l";
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+const pool = new Pool({
+  host: "ec2-54-216-185-51.eu-west-1.compute.amazonaws.com",
+  database: "da03vvrv0hhp7l",
+  user: "lmiadzbwcpstsz",
+  password: "f39cae1f7f55cd3cb16010e924eab745533c1eb9568e04f35ee92ea21ae0afa8",
   ssl: { rejectUnauthorized: false },
 });
 const pgPoolWrapper = {
   async connect() {
     for (let nRetry = 1; ; nRetry++) {
       try {
-        const client = await client.connect();
+        const client = await pool.connect();
         if (nRetry > 1) {
           console.info("Now successfully connected to Postgres");
         }
@@ -34,4 +37,4 @@ const pgPoolWrapper = {
   },
 };
 
-module.exports = client;
+module.exports = pool;
